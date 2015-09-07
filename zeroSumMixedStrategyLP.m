3980627dd5e984216499275cf03514fa1ec788fd
khashab2 <khashab2@illinois.edu>
2015-09-07 10:36:39 -0700

    correcting the code for LP

--- zeroSumMixedStrategyLP.m
+++ zeroSumMixedStrategyLP.m
@@ -4,23 +4,30 @@ A = [4 3 1 4; 2 5 6 3; 1 0 7 0];
 % A = [0 5 -2; -3 0 4; 6 -4 0]; 
 % A = [6 8 3 1 6; 4 2 6 3 5; 2 4 6 4 1; 1 3 2 5 3]; 
 
+% A = -A'; 
+
 n = size(A,1); 
 m = size(A,2); 
 
+totalMaxVal = max(max(abs(A))); 
+
 % compute the first LP 
-f = [zeros(n), zeros(n,1); zeros(1, n) ones(1,1)]; 
-C = [-A'  ones(m, 1); ones(1, n) 0]; 
-b = [zeros(m, 1); 1]; 
-lb = 0; 
-ub = []; 
-Ceq = [];
-beq = []; 
+f = [zeros(n,1); -1]; 
+C = [-A' ones(m, 1)]; 
+b = [zeros(m, 1)]; 
+lb = [zeros(n, 1); -totalMaxVal]; 
+ub = [ones(n, 1); totalMaxVal]; 
+ub = [];
+Ceq = [ones(1, n) 0];
+beq = 1;
 
 % solve the LP 
 disp('starting to solve the lp . . . ')
-x = linprog(f,C,b,Ceq,beq,lb, ub) 
+[x,fval,exitflag,output] = linprog(f,C,b,Ceq,beq,lb,ub);
 
 % the solution 
-disp('The solution  . . . ')
+disp('The solution  . . . '); 
 assert(length(x) == n + 1)
 x(1:n)
+v = x(n+1)
+
