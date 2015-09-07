742cb78c9f33d56f20d7d7b5924f49d6b7ee8ddc
khashab2 <khashab2@illinois.edu>
2015-09-07 01:52:48 -0500

    files renames

--- zeroSumMixedStrategyLP.m
+++ zeroSumMixedStrategyLP.m
@@ -8,15 +8,19 @@ n = size(A,1);
 m = size(A,2); 
 
 % compute the first LP 
-f = [zeroes(n), zeroes(n,1); zeroes(1, n) ones(1,1)]; 
+f = [zeros(n), zeros(n,1); zeros(1, n) ones(1,1)]; 
 C = [-A'  ones(m, 1); ones(1, n) 0]; 
 b = [zeros(m, 1); 1]; 
-lb = 0 
-ub = []
+lb = 0; 
+ub = []; 
+Ceq = [];
+beq = []; 
 
 % solve the LP 
+disp('starting to solve the lp . . . ')
 x = linprog(f,C,b,Ceq,beq,lb, ub) 
 
 % the solution 
+disp('The solution  . . . ')
 assert(length(x) == n + 1)
 x(1:n)
